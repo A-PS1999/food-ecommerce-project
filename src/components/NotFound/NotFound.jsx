5667c3458d32bc86210cac8f4314094d7ecacf76
A-PS1999 <sarnoldparra@gmail.com>
2022-06-27 21:17:31 +0100

    Properly created and styled 404 page with custom image

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { Link, useNavigate } from 'react-router-dom';
 import './NotFound.scss';
 
 export default function NotFound() {
+    const navigate = useNavigate();
+
     return (
-        <div>PLACEHOLDER</div>
+        <div className="not-found-page">
+            <img src="/uh-oh.svg" alt="404: Page not found" className="not-found-page__img" />
+            <h1 className="not-found-page__text">
+                It looks like the page you are looking for doesn't exist.
+            </h1>
+            <div className="not-found-page__btn-group">
+                <Link to="/" className="not-found-page__btn-group__link">
+                    Return to front page
+                </Link>
+                <button className="not-found-page__btn-group__btn" onClick={() => navigate(-1)}>
+                    Previous page
+                </button>
+            </div>
+        </div>
     )
 }
